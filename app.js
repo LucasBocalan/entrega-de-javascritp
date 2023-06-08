a82082dd94f606df5cbe27ed1731beb802c79275
Lucas Bocalandro <Lbocalandro94@gmail.com>
2023-06-08 12:17:39 -0300

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,107 @@
+
+const wordContainer = document.getElementById("wordContainer");
+const StartButton = document.getElementById('StartButton');
+const usedLettersElement = document.getElementById('usedLetters');
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+ctx.canvas.width = 0;
+ctx.canvas.height = 0;
+ 
+const bodyParts = [
+  [4,2,1,1,],
+  [4,3,1,2,],
+  [3,5,1,1,],
+  [5,5,1,1,],
+  [3,3,1,1,],
+  [5,3,1,1,],
+];
+
+let selectedword;
+let usedLetters;
+let mistakes;
+let hits;
+
+
+const wrongLetter () => {
+  addBodyPart(bodyParts[mistakes]);
+
+}
+
+const endGame = () => {
+  document.removeEventListener('keydown', letterEvent);
+  StartButton.style.display = 'block' ;
+}
+
+const correctLetter = letter => {
+  const { children } = wordContainer;
+  for (let i = 0; i < children.length; i++) {
+    if(children[i].innerHTML === letter){ 
+    (children)[i].classList.toggle('hidden');
+    hits++;
+    }
+  }
+  if(hits === selectedWord.length) engGame();
+
+}
+
+const letterInput = letter => {
+  if(selectedWord.includes(letter)) {
+    correctLetter(letter);
+    else {
+      wrongLetter();
+
+    }
+  }
+};
+
+const letterEvent = event => {
+  let newLetter = event.key.toUpperCase();
+  if(newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter)){
+    letterInput(newLetter);
+
+  };
+
+};  
+
+const drawWord = () => {
+  selectedword.forEach(letter => {
+    const lettersElement = document.createElement('span');
+    lettersElement.innerHTML = letter.toUpperCase();
+    lettersElement.classList.add('letter');
+    lettersElement.classList('hidden');
+    wordContainer.appendChild(letterElement);
+  });
+}
+
+const selectRandomWord = () => {
+  let word = words[Math.floor((Math.random() * words.length))].toUpperCase();
+  selectedWord = word.split('');
+};
+
+const drawHangMan = () => {
+  ctx.canvas.width = 120;
+  ctx.canvas.height = 160;
+  ctx.scale(20, 20);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = '#d95d39'
+  ctx.fillRect(0, 7, 4, 1);
+  ctx.fillRect(1, 0, 1, 8); 
+  ctx.fillRect(2, 0, 3, 1);
+  ctx.fillRect(4, 1, 1, 1);
+};
+
+const startGame = () => {
+    usedLetters = {}
+    mistakes = 0;
+    hits = 0;
+    wordContainer.innerHTML = '';
+    usedLettersElement.innerHTML = '';
+    StartButton.style.display = 'none';
+    drawHangMan();
+    selectRandomWord();
+    drawWord();
+    document.addEventListener('keydown', letterEvent);
+  };
+
+StartButton.addEventListener('click', startGame);    
